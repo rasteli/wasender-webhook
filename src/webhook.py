2d5dffbe26a3b1a88e448e173ea9b452028c3d46
Gabriel de Lima Rasteli <gabriel.rasteli@morlan.com.br>
2025-11-05 09:48:40 -0300

    Logar menasgem

--- src/webhook.py
+++ src/webhook.py
@@ -1,37 +1,45 @@
-import os
+import os, dotenv
+dotenv.load_dotenv()
+
+from datetime import datetime
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
-
 WEBHOOK_SECRET = os.getenv('WASENDER_WEBHOOK_SECRET')
 
 
 def verify_signature(req):
-    signature = req.headers.get("x-webhook-signature")
+  signature = req.headers.get("x-webhook-signature")
 
-    if not signature or signature != WEBHOOK_SECRET:
-        return False
+  if not signature or signature != WEBHOOK_SECRET:
+    return False
 
-    return True
+  return True
 
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
-    if not verify_signature(request):
-        return jsonify({"error": "Invalid signature"}), 401
+  if not verify_signature(request):
+    return jsonify({"error": "Invalid signature"}), 401
 
-    payload = request.json
-    print("Received webhook event:", payload.get("event"))
+  payload = request.json
+  print("Received webhook event:", payload.get("event"))
 
-    event_type = payload.get("event")
+  event_type = payload.get("event")
 
-    if event_type == "messages.upsert":
-        data = payload.get("data", {})
-        key = data.get("key", {})
-        print("New message received:", key.get("id"))
+  if event_type == "messages.upsert":
+    data = payload["data"]
+    timestamp = payload["timestamp"]
+    key = data["key"]
+    message = data["message"]["conversation"]
 
-    return jsonify({"received": True}), 200
+    from_ = key.get('remoteJid') if not key["fromMe"] else 'me'
+    sent_date = datetime.fromtimestamp(timestamp).strftime('%d-%m-%Y %H:%M')
+
+    print(f"New message from {from_}: {message} ({sent_date})")
+
+  return jsonify({"received": True}), 200
 
 
 if __name__ == "__main__":
-    app.run(port=3000)
+  app.run(port=3000)
